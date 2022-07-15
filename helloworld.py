3b968e0c0fa244a4c7a23ba80f2cb78042a41c2f
BaituBaitu <dengsiyuan.yoko@foxmail.com>
2022-07-15 15:09:54 +0800

    a commit message form pycharm

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：pylearn 
+@File    ：helloworld.py
+@Author  ：BaituBaitu
+@Date    ：2022/7/15 15:06 
+'''
+
+# to add a .py file to pylearn repository and push it to Github
